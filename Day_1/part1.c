3c9674713c6030f7272fcd9361e895f2ab636d78
YounesBenketira <admin@younesbenketira.com>
2024-06-17 04:15:26 -0400

    Initial commit

---
+++ Day_1/part1.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	FILE *file = fopen("input", "r");
+	
+	if (file == NULL) {
+		perror("Error opening file");
+        	return 1;
+	}
+
+	char buffer[256];
+	int sum = 0;
+	while (fgets(buffer, sizeof(buffer), file) != NULL) {
+		int first = -1;
+		int second = -1;
+		for(int i = 0; buffer[i] != '\0'; i++) {
+			if (buffer[i] >= '0' && buffer[i] <= '9') {
+				int digit = buffer[i] - '0';
+				if(first == -1) {
+					first = digit;
+				} else {
+					second = digit;
+				}
+			}
+		}
+
+		char numsAsStr[3] = "";
+		if (first != -1) {
+			if (second == -1) {
+				sprintf(numsAsStr, "%d%d", first, first);
+			} else {
+				sprintf(numsAsStr, "%d%d", first, second);
+			}
+			int lineTotal = atoi(numsAsStr);
+			sum += lineTotal;
+		}
+	}
+
+	printf("Total = %d\n", sum);
+	
+	fclose(file);
+	return 0;
+}
---
+++ Day_1/part2.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "trie.h"
+ 
+bool isDigit(char c) {
+	return c <= '9' && c >= '0';
+}
+
+int main() {
+	/* INITIALIZE TRIE */
+	Node *root = createNode();
+		
+	char *numbers[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	for (int i = 0; i < 9; i++) {
+		addWord(root, numbers[i], i + 1);
+	}
+
+	/* PARSE FILE */
+	FILE *file = fopen("input", "r");
+	
+	int sum = 0;	
+	char buffer[256];
+	while (fgets(buffer, sizeof(buffer), file) != NULL) {	
+		char lineTotal[3] = {0};
+		for (int i = 0; buffer[i] != '\0'; i++) {
+			char value = '\0';
+				
+			if (isDigit(buffer[i])) {
+				value = buffer[i];
+			} else {
+				int num = getWordValue(root, i, buffer);
+				if (num > 0) {
+					value = '0' + num;
+				}
+			}
+		
+			if (value != '\0') {
+				if (lineTotal[0] == 0) {
+					lineTotal[0] = value;
+				} else {
+					lineTotal[1] = value;
+				}
+			}
+		}
+		
+		// If only ONE number found, use it for both digits (i.e 1 -> 11)		
+		if (lineTotal[0] > 0 && lineTotal[1] == 0) {
+			lineTotal[1] = lineTotal[0];
+		}
+		
+		sum += atoi(lineTotal);
+	}
+		
+	fclose(file);
+	freeTrie(root);
+
+	printf("Total = %d\n", sum);
+	return 0;
+}
