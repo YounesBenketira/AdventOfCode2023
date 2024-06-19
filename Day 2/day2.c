b33ae26b8c55b74b1303aa0cf91a7eceeb266516
YounesBenketira <admin@younesbenketira.com>
2024-06-18 22:40:09 -0400

    Impl. rough draft

--- Day 2/day2.c
+++ Day 2/day2.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int isDigit(char c) {
 	return c <= '9' && c >= '0';
@@ -12,43 +13,60 @@ int main() {
 		return 0;
 	}	
 
+	int sum = 0;
+
 	char buffer[256];
 	int gameNumber = 1;
 	while(fgets(buffer, sizeof(buffer), file) != NULL) {
-		int *counts[3] = {0};
-		char *number[3] = {'\0'};
+		int counts[3] = {0};
 		for (int i = 0; buffer[i] != '\n'; i++) {
-			// 1. Number found
-			// 2. Space found after number
-			// 3. Increment count of color
+			char diceCount[3] = {'\0'};
 			if (isDigit(buffer[i]) && buffer[i + 1] == ' ') {
 				char color = buffer[i + 2];
 				int num = buffer[i] - '0';
 				
+				if (isDigit(buffer[i - 1])) {
+					diceCount[0] = buffer[i - 1];
+					diceCount[1] = buffer[i];
+				} else {
+					diceCount[0] = buffer[i];
+				}
+				
 				int index = -1;
-				if (color == 'r') {
-					index = 0;	
-				} 
-				else if (color == 'b') {
-					index = 1;
-				}
-				else if (color == 'g') {
-					index = 2;
-				}
-				else {
-					printf("BAD COLOR FOUND %c", color);
-					return 0;
+				switch(color) {
+					case 'r':
+						index = 0;
+						break;
+					case 'b':
+						index = 1;
+						break;
+					case 'g':
+						index = 2;
+						break;
+					default:
+						printf("Invalid color %c", color);
+						break;
 				}
 
-				counts[index] += num;
+				counts[index] += atoi(diceCount);
 
-				printf("%d %c  ", num, color);
+				// printf("%d %c ", atoi(diceCount), color);
 			} 
 		}
+
+		printf("%d red, %d blue, %d green \n", counts[0], counts[1], counts[2]);
+		
+		// 0:R, 1:G, 2:B
+		if (counts[0] <= 12 && counts[1] <= 13 && counts[2] <= 14) {
+			printf("\nGame %i is valid\n", gameNumber);
+			sum += gameNumber;
+		}
+		
 		printf("\n");
 		gameNumber++;
 	}
-
+	
+	printf("\nTotal = %i\n", sum);
 	fclose(file);
 
 	return 0;
