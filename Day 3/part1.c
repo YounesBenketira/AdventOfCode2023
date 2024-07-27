b85e5a8772c1ecc1513b3c1baaa832c2d4b701b1
YounesBenketira <admin@younesbenketira.com>
2024-07-26 21:44:23 -0400

    Impl. number parsing properly

--- Day 3/part1.c
+++ Day 3/part1.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-
-int isDigit(char c) {
-	return c <= '9' && c >= '0';
-}
+#include <stdlib.h>
+#include <ctype.h>
 
 void main() {
 	FILE *file = fopen("input.txt", "r");
@@ -12,14 +10,31 @@ void main() {
 		return;
 	}
 
+    int lineNumber = 1;
 	char buffer[256];
 	while(fgets(buffer, sizeof(buffer), file) != NULL) {
-		char numTracker = 's'; 
-		for(int i = 0; buffer[i] != '\n'; i++) {
-			if (	
-		}
-		printf("%s\n", buffer);
+		char *current = &buffer[0];
+        
+        for (int i = 0; buffer[i] != '\n' && buffer[i] != '\0'; ) {
+            if (isdigit(buffer[i])) {
+                char *end;
+                int number = strtol(&buffer[i], &end, 10);
+                int end_index = end - buffer - 1;
+
+                printf("Number: %i   Start: %i   End: %i\n", number, i, end_index);
+                
+                i = end_index + 1;
+            } else {
+                
+
+                i++;
+            }
+        } 
+
+        lineNumber++;
 	}
+
+    fclose(file);
 
 	printf("DONE\n");
 	return;
