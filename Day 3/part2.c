db820a5165d8da7e115defc83ea01fe2d519a08e
YounesBenketira <admin@younesbenketira.com>
2024-08-11 14:22:03 -0400

    Impl. new hashmap for part 2 of day 3

--- Day 3/part2.c
+++ Day 3/part2.c
@@ -2,35 +2,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#include "hashmap.h"
+#include "hashmap_2.h"
 
 /*
 * Parses file and maps all symbols in a line to an array of their indicies
 */
-void populateSymbolMap(HashMap *symbolMap, FILE *file) {
+void populateNumberMap(Map *numberMap, FILE *file) {
     char buffer[256];
     int lineNumber = 0;
     while(fgets(buffer, sizeof(buffer), file) != NULL) {
-        for (int i = 0; buffer[i] != '\n' && buffer[i] != '\0'; i++ ) {
-            if (isdigit(buffer[i]) || buffer[i] == '.') {
-                continue;
-            }
-
-            insert(symbolMap, lineNumber, i);
-        }
-
-        lineNumber++;
-    }
-}
-
-/**
-* Finds numbers and adds them to the sum if eligible 
-*/
-int calculateSum(HashMap symbolMap, FILE *file) {
-    char buffer[256];
-    int lineNumber = 0;
-    int sum = 0;
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
         for (int i = 0; buffer[i] != '\n' && buffer[i] != '\0'; i++ ) {
             if (!isdigit(buffer[i])) {
                 continue;
@@ -41,33 +21,13 @@ int calculateSum(HashMap symbolMap, FILE *file) {
             int number = strtol(&buffer[i], &end, 10);
             int endIndex = end - buffer - 1;
            
-            /* Parse from the line above to the line below for symbols that are adjacent */
-            for (int j = lineNumber - 1; j <= lineNumber + 1; j++) {
-                int symbolIndiciesArrayLength = 0;
-                int *symbolIndiciesForLine = get(&symbolMap, j, &symbolIndiciesArrayLength);
-                
-                if (symbolIndiciesForLine == NULL) {
-                    continue;
-                }
-                
-                /* Loop through symbols in the line and determine if they are adjacent */
-                for (int k = 0; k < symbolIndiciesArrayLength; k++) {
-                    if (
-                        symbolIndiciesForLine[k] >= i - 1 &&
-                        symbolIndiciesForLine[k] <= endIndex + 1
-                    ) {
-                        sum += number;
-                    }
-                }
-            } 
+            insert(numberMap, number, i, endIndex, lineNumber);
             
-            i = endIndex + 1;
+            i = endIndex;            
         }
-
+      
         lineNumber++;
     }
-
-    return sum;
 }
 
 void main() {
@@ -78,16 +38,21 @@ void main() {
 		return;
 	}
 
-    HashMap symbolMap;
-    init_hashmap(&symbolMap);
+    Map numberMap;
+    initMap(&numberMap);
     
-    populateSymbolMap(&symbolMap, file);
+    populateNumberMap(&numberMap, file);
     
+    int numsInLineSize;
+    int *numbersInLineZero = getNumbersInLine(&numberMap, 0, &numsInLineSize);
+    
+    free(numbersInLineZero);
+
     fseek(file, 0, SEEK_SET);
 
-    int sum = calculateSum(symbolMap, file);
+    //int sum = calculateSum(symbolMap, file);
     fclose(file);
-    printf("DONE, Answer: %i\n", sum);
+    printf("DONE, Answer: %i\n", 0);
 	return;
 }
 
