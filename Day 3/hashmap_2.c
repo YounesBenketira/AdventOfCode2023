49a764d90b9814bc30af6fd0c53a1f36d18c7076
YounesBenketira <admin@younesbenketira.com>
2024-08-11 14:22:15 -0400

    add missing files

---
+++ Day 3/hashmap_2.c
@@ -0,0 +1,81 @@
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "hashmap_2.h"
+
+void initMap(Map *map) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        map->table[i].key = 0;
+        map->table[i].isOccupied = false; 
+    }
+}
+
+Entry* get(Map *map, int key) {
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (map->table[i].key != key) {
+            continue;
+        }
+
+        return &(map->table[i]);
+    }
+
+    return NULL;
+}
+
+int* getNumbersInLine(Map *map, int lineNumber, int *returnSize) {
+    int *nums = malloc(256 * sizeof(int));
+    int size = 0;
+
+    if (nums == NULL) {
+        *returnSize = 0;
+        return NULL;
+    }
+
+    for (int i = 0; i < TABLE_SIZE; i++) {
+        if (map->table[i].key != 0) {
+            printf("{%d} ", map->table[i].key);
+        }
+        
+        if (map->table[i].lineNumber != lineNumber) {
+            continue;
+        }
+
+        nums[size] = map->table[i].key;
+        
+        size++;
+        if (size >= 256) break;
+    }
+
+    for (int i = 0; i < size; i++) {
+        if (nums[i] == 0) continue;
+        printf("%d ", nums[i]);        
+    }
+    printf("\n");
+
+    *returnSize = size;
+    return nums;
+}
+
+unsigned int hash(int key) {
+    key = ((key >> 16) ^ key) * 0x45d9f3b;
+    key = ((key >> 16) ^ key) * 0x45d9f3b;
+    key = (key >> 16) ^ key;
+
+    return key % TABLE_SIZE;
+}
+
+void insert(Map *map, int key, int start, int end, int lineNumber) {
+    int index = hash(key);
+
+    printf("Inserting %i [%i] [%i]\n", key, lineNumber, index);
+    if (map->table[index].isOccupied) {
+        printf("Collision Happened, unhandled\n");
+    }
+
+    map->table[index].key = key;
+    map->table[index].start = start;
+    map->table[index].end = end;
+    map->table[index].lineNumber = lineNumber;
+    map->table[index].isOccupied = true;
+}
---
+++ Day 3/hashmap_2.h
@@ -0,0 +1,25 @@
+#ifndef HASHMAP_2_H
+#define HASHMAP_2_H
+
+#include <stdbool.h>
+
+#define TABLE_SIZE 1024
+
+typedef struct {
+    int key;
+    int start; 
+    int end;
+    int lineNumber;
+    bool isOccupied;
+} Entry;
+
+typedef struct {
+    Entry table[TABLE_SIZE];
+} Map;
+
+void initMap(Map *map);
+Entry* get(Map *map, int key);
+int* getNumbersInLine(Map *map, int lineNumber, int *returnSize);
+void insert(Map *map, int key, int start, int end, int lineNumber);
+
+#endif 
