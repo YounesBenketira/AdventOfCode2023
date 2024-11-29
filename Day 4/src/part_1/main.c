b02e2196b1c665e84137349ac7b7970f9dd3df98
YounesBenketira <admin@younesbenketira.com>
2024-11-28 21:59:32 -0500

    Impl. makefile and basic parsing

--- Day 4/src/part_1/main.c
+++ Day 4/src/part_1/main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World");
+    FILE *file = fopen("../test_input.txt", "r");
+
+	if (file == NULL) {
+		perror("Failed to open file");
+		return 0;
+	}
+
+    char buffer[256]; 
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        for (int i = 0; buffer[i] != '\n' && buffer[i] != '\0'; i++ ) {
+            printf("%c", buffer[i]);
+        }
+        printf("\n");
+    }
+
+    printf("Done");
         
     return 0;
 }
