17010689c3da84787a58c73b4987c44bed6f8f17
YounesBenketira <admin@younesbenketira.com>
2024-12-02 22:03:13 -0500

    Impl. separate function and cleanup makefile

--- Day 4/src/part_1/main.c
+++ Day 4/src/part_1/main.c
@@ -1,22 +1,41 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    FILE *file = fopen("../test_input.txt", "r");
+#define FILE_PATH "/home/younes/Documents/Code/Personal/AdventOfCode2023/Day 4/src/test_input.txt"
 
-	if (file == NULL) {
-		perror("Failed to open file");
-		return 0;
-	}
-
-    char buffer[256]; 
-    while (fgets(buffer, sizeof(buffer), file) != NULL) {
-        for (int i = 0; buffer[i] != '\n' && buffer[i] != '\0'; i++ ) {
-            printf("%c", buffer[i]);
-        }
-        printf("\n");
+/**
+*
+*/
+int read_file_contents(const char* filepath) {
+    FILE *file = fopen(filepath, "r");
+    
+    if (file == NULL) {
+        fprintf(stderr, "Error: Unable to open file %s\n", filepath);
+        
+        return -1;
     }
 
-    printf("Done");
-        
-    return 0;
+    char buffer[256];
+    int line_count = 0;
+
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+        printf("Line %d: %s", ++line_count, buffer);
+    }
+
+    fclose(file);
+
+    return line_count;
+}
+
+int main() {
+    int result = read_file_contents(FILE_PATH);
+
+    if (result < 0) {
+        return EXIT_FAILURE;
+    }
+
+    printf("Total lines read: %d\n", result);
+
+    return EXIT_SUCCESS;
 }
